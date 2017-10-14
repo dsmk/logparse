546899ffeece5b911c71f8bacde6329f23a520c1
David King <dsmk@bu.edu>
2017-10-14 00:16:15 -0400

    adding simple test routinges to the script.

---
+++ logparse_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+  "testing"
+)
+
+var testIPData = []map[string]string {
+  { "name": "10net", "net": "10.0.0.0/8", "track": "hosts,uri" },
+  { "name": "localhost", "net": "127.0.0.1/32", "track": "uri" },
+}
+
+func testIPRanges () ([]network) {
+  ipranges, _ := initIPRanges(testIPData)
+
+  return ipranges
+}
+
+func TestInitIPRanges (t *testing.T) {
+  ipranges := testIPRanges()
+
+  //t.Errorf("ipranges=%+v", ipranges)
+  for num, item := range ipranges {
+    t.Logf("item[%d]=%s name=%s -> %s", num, item, item.name, testIPData[num]["name"])
+    if item.name != testIPData[num]["name"] {
+      t.Errorf("build failed: name should be %s but is %s", testIPData[num]["name"], item.name)
+    }
+  }
+}
+
+func TestFindNetwork (t *testing.T) {
+  ipranges := testIPRanges()
+
+  ip, trackH, trackU, name := findNetwork(ipranges, "10.0.0.1")
+
+  if ip != "10.0.0.1" {
+    t.Errorf("test ip != 10.0.0.1")
+  }
+  if name != "10net" {
+    t.Errorf("test range == %s, %b, %b, %s", ip, trackH, trackU, name)
+  }
+  //t.Errorf("Testing having a test fail %d\n", 1)
+}
+
+func TestMainParseOK (t *testing.T) {
+  line := `67.249.231.2 - - [01/Sep/2017:00:00:08 -0400] "GET /met/wp-includes/js/wp-embed.min.js?ver=4.6.6 HTTP/1.1" 200 1403 0.007192 0.000000 0.000000 "http://www.bu.edu/met/programs/graduate/arts-administration/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36" 10673 + WajbSArxHDYAACmxCSUAAAVW 128.197.26.35 off:http`
+  expect := map[string]string {
+    "ip": "67.249.231.2",
+    "base_uri": "/met/wp-includes/js/wp-embed.min.js",
+    "uri": "/met/wp-includes/js/wp-embed.min.js?ver=4.6.6",
+    "browser": `"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"`,
+    "protocol": `HTTP/1.1`,
+  }
+
+  item := ParseAccess(1, line)
+
+  for k, v := range expect {
+    if item[k] != v {
+      t.Errorf("%s: parsed (%s) instead of (%s)", k, item[k], v)
+    }
+  }
+
+  //t.Errorf("returned %+v", item)
+}
+
